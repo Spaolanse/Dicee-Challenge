f42d6e24b49e7f79e6eaa6e5f74872e19cccb73c
Spaolanse <gustavospaolanse@gmail.com>
2024-06-06 13:14:18 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,8 +3,6 @@ const numTwo = Math.floor(Math.random() * 6 + 1);
 const diceOne = document.getElementById('img1');
 const diceTwo = document.getElementById('img2');
 const winner = document.getElementById('winner');
-console.log(numOne);
-console.log(numTwo);
 
     if(numOne > numTwo) {
         winner.innerText = '🚩 Player 1 Wins!';
@@ -14,31 +12,10 @@ console.log(numTwo);
         winner.innerText = 'Draw!';
     }
 
-    if (numOne === 1) {
-        diceOne.src = 'images/dice1.png';
-    } else if (numOne === 2){
-        diceOne.src = 'images/dice2.png';
-    } else if (numOne === 3){
-        diceOne.src = 'images/dice3.png';
-    } else if (numOne === 4){
-        diceOne.src = 'images/dice4.png';
-    } else if (numOne === 5){
-        diceOne.src = 'images/dice5.png';
-    } else {
-        diceOne.src = 'images/dice6.png';
-    }
+    if(numOne >= 1 && numOne < 7){
+        diceOne.src = `images/dice${numOne}.png`;
+    };
 
-
-    if(numTwo === 1) {
-        diceTwo.src = 'images/dice1.png';
-    } else if(numTwo === 2){
-        diceTwo.src = 'images/dice2.png';
-    } else if(numTwo === 3){
-        diceTwo.src = 'images/dice3.png';
-    }else if(numTwo === 4){
-        diceTwo.src = 'images/dice4.png';
-    }else if(numTwo === 5){
-        diceTwo.src = 'images/dice5.png';
-    }else {
-        diceTwo.src = 'images/dice6.png';
-    }
+    if(numTwo >= 1 && numTwo < 7){
+        diceTwo.src = `images/dice${numTwo}.png`;
+    };
